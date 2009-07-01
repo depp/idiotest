95af6ae60d02326124000a912211db9f044ee495
Dietrich Epp <depp@zdome.net>
2009-07-01 14:11:51 -0700

    Updated header comments.

--- idiotest/fail.py
+++ idiotest/fail.py
@@ -1,4 +1,4 @@
-# IdioTest - idiotest/env.py
+# IdioTest - idiotest/fail.py
 # Copyright 2009 Dietrich Epp <depp@zdome.net>
 # This source code is licensed under the GNU General Public License,
 # Version 3. See gpl-3.0.txt for details.
--- selftest/check_output.py
+++ selftest/check_output.py
@@ -1,4 +1,4 @@
-# IdioTest - selftest/selftest.py
+# IdioTest - selftest/check_output.py
 # Copyright 2009 Dietrich Epp <depp@zdome.net>
 # This source code is licensed under the GNU General Public License,
 # Version 3. See gpl-3.0.txt for details.
