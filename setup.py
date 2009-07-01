d2846a747d002765acb0d5a5866abb12589052c3
Dietrich Epp <depp@zdome.net>
2009-07-01 14:41:31 -0700

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='IdioTest',
+      version='0.1',
+      description='Idiotic testing framework',
+      author='Dietrich Epp',
+      author_email='depp@zdome.net',
+      packages=['idiotest'])
