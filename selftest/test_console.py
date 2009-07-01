9b488f05c2fcab95db1a8a5e022d807d3580bb45
Dietrich Epp <depp@zdome.net>
2009-07-01 14:36:22 -0700

    Added test for console write flushing.

---
+++ selftest/test_console.py
@@ -0,0 +1,12 @@
+# IdioTest - selftest/test_console.py
+# Copyright 2009 Dietrich Epp <depp@zdome.net>
+# This source code is licensed under the GNU General Public License,
+# Version 3. See gpl-3.0.txt for details.
+import time
+
+def test_sleep_succeed():
+    time.sleep(1)
+
+def time_sleep_fail():
+    time.sleep(1)
+    fail("supposed to fail")
