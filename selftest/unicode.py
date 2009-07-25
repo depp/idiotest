eda47413e7302c24a778b2bf2d2e10ae02a661e6
Dietrich Epp <depp@zdome.net>
2009-07-24 22:41:27 -0700

    Fixed source code header.

--- selftest/unicode.py
+++ selftest/unicode.py
@@ -1,2 +1,6 @@
+# IdioTest - selftest/unicode.py
+# Copyright 2009 Dietrich Epp <depp@zdome.net>
+# This source code is licensed under the GNU General Public License,
+# Version 3. See gpl-3.0.txt for details.
 def utest_FAIL():
     check_output(['cat'], '\xff\n', '\xff\fe\n')
