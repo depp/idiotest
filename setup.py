b1563fb01ece4b4c01641b01ab33fd5993d4f16a
Dietrich Epp <depp@zdome.net>
2012-01-06 03:13:51 -0800

    Scrub references to "Idiotic" testing framework

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 setup(name='IdioTest',
       version='0.1',
-      description='Idiotic testing framework',
+      description='Simple testing framework',
       author='Dietrich Epp',
       author_email='depp@zdome.net',
       packages=['idiotest'])
